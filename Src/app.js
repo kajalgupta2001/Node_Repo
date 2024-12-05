e8a5b76f4390ef7b235d389269a5b57a80b4d229
Kajal Gupta <kajal.gupta96011@gmail.com>
2024-12-05 11:37:01 +0530

    added number

--- Src/app.js
+++ Src/app.js
@@ -8,6 +8,7 @@ const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3000;
 
+let a = 10
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
